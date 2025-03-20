7a2a5f671147880a5f4ea3c200f87bbf95b5b5e1
Victor Rodrigues <victorassisrd@gmail.com>
2025-03-20 01:06:36 -0300

    feat: :sparkles: Cálculo de previsão de ações
    Cálculo de previsão de ações

--- api/main.py
+++ api/main.py
@@ -1,14 +1,28 @@
 from typing import Optional
-
 from fastapi import FastAPI
+from api.core.models import PredictPayload
+from api.services.prediction import PredictionService
+from api.services.weights import WeightsService
 
-app = FastAPI()
-
+app = FastAPI(
+    title="Minha API de Investimentos",
+    description="API para calcular retorno de ações",
+    version="1.0.0",
+    docs_url="/docs",        # Rota do Swagger UI (pode mudar)
+    redoc_url="/redoc",      # Rota do Redoc (pode mudar ou desativar)
+)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Serviço no ar"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/predict")
+def read_item(payload:PredictPayload):
+    
+    weights_service = WeightsService(payload.ticker)
+
+    try:
+        prediction_service = PredictionService(weights_service)
+        return { "prediction": prediction_service.predict(payload) }
+    except Exception as e:
+        return { "error" : str(e) }
